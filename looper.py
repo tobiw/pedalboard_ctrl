c5c706f8a88a55169781099d638161ff6fe0daba
Tobi Wulff <tobi@swulff.de>
2020-02-05 17:37:22 +1300

    Implement SooperlooperOscInterface class

--- looper.py
+++ looper.py
@@ -26,6 +26,38 @@ class LooperOscServer:
     def _osc_cb(self, *args):
         print("LooperOscServer._osc_cb: " + str(args))
 
+    @property
+    def uri(self):
+        return 'osc.udp://localhost:{}'.format(self._port)
+
+
+class SooperlooperOscInterface:
+    def __init__(self, port):
+        self._osc_client = udp_client.SimpleUDPClient('127.0.0.1', port)
+
+        # OSC server for receiving responses and updates from sooperlooper
+        self._osc_server = LooperOscServer()
+
+        # Start OSC server and register for updates from sooperlooper
+        self._osc_server.start()
+
+    def register_update(self, loop, ctrl, return_path, auto_update=False):
+        """/sl/0/register_update or /register_update, or ...register_auto_update"""
+        prefix = ''
+        if loop is not None or loop == -1:
+            prefix += '/sl/{}'.format(loop)
+
+        update_method = 'auto_update' if auto_update else 'update'
+
+        self._osc_client.send_message(prefix + '/register_' + update_method, [ctrl, self._osc_server.uri, '/get_response'])
+
+    def get(self, loop, ctrl):
+        """/sl/0/get (e.g. 'state') or /get (e.g. 'tempo')"""
+        prefix = ''
+        if loop is not None or loop == -1:
+            prefix += '/sl/{}'.format(loop)
+        self._osc_client.send_message(prefix + '/get', [ctrl, self._osc_server.uri, '/get_response'])
+
 
 class Looper:
     def __init__(self):
@@ -41,11 +73,9 @@ class Looper:
         # Thread to run sooperlooper in the background
         self._sl_thread = threading.Thread(target=self._run_sooperlooper)
 
-        # OSC client to send commands to sooperlooper
-        self._osc_client = udp_client.SimpleUDPClient('127.0.0.1', self._sl_config['osc_port'])
-
-        # OSC server for receiving responses and updates from sooperlooper
-        self._osc_server = LooperOscServer()
+        # OSC interface to send commands to sooperlooper
+        self._sooperlooper_osc = SooperlooperOscInterface(self._sl_config['osc_port'])
+        self._sooperlooper_osc.get(0, 'state')
 
     def _run_sooperlooper(self):
         cmd = [
@@ -69,9 +99,6 @@ class Looper:
             subprocess.call(['jack_connect', 'sooperlooper:common_out_{}'.format(i), 'system:playback_{}'.format(i)])
 
         time.sleep(3)
-
-        # Start OSC server and register for updates from sooperlooper
-        self._osc_server.start()
 
         # TODO: keep attempting to connect (so that it works if plugged in later)
         subprocess.call(['aconnect', 'USBMIDI', 'sooperlooper'])

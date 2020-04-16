3c4289cbb39d4ba25b3b931228012ce1a556a323
Tobi Wulff <tobi@swulff.de>
2020-04-16 13:15:24 +1200

    Move main program logic into App class

--- midi_receiver.py
+++ midi_receiver.py
@@ -1,37 +1,50 @@
+import logging
 from rtmidi import RtMidiIn, RtMidiOut
 
 
 class MidiReceiver:
-    def __init__(self):
-        self._midi_in, self._midi_out = RtMidiIn(), RtMidiOut()
-        self._connect_midi('USBMIDI')
-        self._midi_in.setCallback(self._midi_message_cb)
+    """
+    Handles incoming MIDI messages from attached controllers or instruments.
 
-    def _connect_midi(self, midi_controller):
+    Incoming messages can be remapped to other MIDI events, OSC, or trigger
+    events inside the app.
+    """
+    def __init__(self, usb_device_name, app):
+        self._log = logging.getLogger(__name__)
+        self._midi_in, self._midi_out = RtMidiIn(), RtMidiOut()
+        self._connect_midi(usb_device_name)
+        self._midi_in.setCallback(self._midi_message_cb)
+        self._app = app
+
+    def _connect_midi(self, usb_device_name):
         def find_port(ports, name):
             for i, p in enumerate(ports):
                 if p.startswith(name):
                     return i
             return None
 
-        # Find the MIDI In port the Arduino Micro is connected to
-        arduino_port = find_port([self._midi_in.getPortName(i) for i in range(self._midi_in.getPortCount())], midi_controller)
-        if arduino_port is None:
-            raise ValueError('Could not find "Arduino Micro" MIDI port')
+        # Find the MIDI In port
+        port = find_port([self._midi_in.getPortName(i) for i in range(self._midi_in.getPortCount())], usb_device_name)
+        if port is None:
+            raise ValueError('Could not find "{}" MIDI port'.format(usb_device_name))
 
-        print("MidiIn connecting to {}".format(arduino_port))
-        self._midi_in.openPort(arduino_port)
+        self._log.info("MidiIn connecting to {}".format(port))
+        self._midi_in.openPort(port)
 
-        # Find the MIDI Out port the Arduino Micro is connected to
-        arduino_port = find_port([self._midi_out.getPortName(i) for i in range(self._midi_out.getPortCount())], midi_controller)
-        assert arduino_port is not None
-        print("MidiOut connecting to {}".format(arduino_port))
-        self._midi_out.openPort(arduino_port)
+        # Find the MIDI Out port
+        port = find_port([self._midi_out.getPortName(i) for i in range(self._midi_out.getPortCount())], usb_device_name)
+        assert port is not None
+        self._log.info("MidiOut connecting to {}".format(port))
+        self._midi_out.openPort(port)
 
     def _midi_message_cb(self, msg):
-        print('MIDI CB: %s' % msg)
+        ch = msg.getChannel()
+        cc = msg.getControllerNumber()
 
+        self._log.debug('Received MIDI message: {} {}'.format(ch, cc))
 
-m = MidiReceiver()
-while True:
-    pass
+        # TODO: map (ch, cc, value) to event
+        if ch == 2:
+            if cc == 117:
+                # event-type (0:MIDI input), target(0:presets), cc
+                self._app.send_event(0, 0, 117)
